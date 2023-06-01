1aa0c27d594d9f24a643bd104a806da7f7d499a0
max <max@luci.com>
2023-05-31 23:02:45 -0400

    update

--- nuxt-server/pages/index.vue
+++ nuxt-server/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
+    <pre>{{ foo }}</pre>
     <List :posts="data.articles" />
   </div>
 </template>
 
 <script setup>
 const config = useRuntimeConfig()
-
+const foo = useFoo()
+console.log('foo', foo)
 const apiUrl = `https://newsapi.org/v2/top-headlines?country=ca&apiKey=${config.public['newsKey']}`
 let { data: data } = await useFetch(apiUrl)
 </script>
